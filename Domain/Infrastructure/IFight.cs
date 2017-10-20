11fe7312c688210dfd2ade1d54ec8aaaa85ba1b9
VitorCioletti <ciolettiv@gmail.com>
2017-10-20 14:31:52 -0200

    Update IFight.cs

--- Domain/Infrastructure/IFight.cs
+++ Domain/Infrastructure/IFight.cs
@@ -2,7 +2,7 @@
 {
     public interface IFight
     {
-        //This interface is declares at the domain, altought, it is implemented at infrastructure layer.
+        //This interface is declared at the domain, altought, it is implemented at infrastructure layer.
         void Attack();
         void PrepareStance();
         void Defend();
